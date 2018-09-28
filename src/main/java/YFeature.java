4c686764439897e525b0a81ee2a991c0e2bffd9b
Ciprian Nicuta <ciprian.nicuta@endava.com>
2018-09-28 14:45:29 +0300

    added new additional functionality

--- src/main/java/YFeature.java
+++ src/main/java/YFeature.java
@@ -2,5 +2,6 @@
 public class YFeature {
     public static void main(String[] args) {
         System.out.println("hello from feature/yFeature");
+        System.out.println("some additional functionality");
     }
 }
